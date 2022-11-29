38ad3829e78ed3c781f40fad5972ca64d55f5eb5
ecarpenterfromalabama <u964010154@byui.edu>
2022-11-29 09:56:42 -0600

    Completed footer

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,10 @@ function toggleMenu() {
       x.className = "topnav";
     }
   }
+
+// Last Modified Date
+let lastMod = new Date(document.lastModified);
+let currentCopy = new Date().getFullYear();
+
+let modifiedDate = document.querySelector(".last-modified");
+modifiedDate.innerHTML = lastMod.toLocaleString();
