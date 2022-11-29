596bca3cb63c851b7cf7e50765f4504e592f3b54
ecarpenterfromalabama <u964010154@byui.edu>
2022-11-29 12:15:51 -0600

    Added more visuals to CTA section

--- scripts/index.js
+++ scripts/index.js
@@ -45,3 +45,20 @@ if ("IntersectionObserver" in window) {
       
     });
   }
+
+  // Smoothies animation
+const ctaEl = document.getElementById("cta");
+const bSmoothieEl = document.getElementById("blue-smoothie");
+const oSmoothieEl = document.getElementById("orange-smoothie");
+
+ctaEl.addEventListener('mouseenter', () =>{
+  bSmoothieEl.classList.add("bounce-blue");
+  setTimeout(()=>{
+    oSmoothieEl.classList.add("bounce-orange")
+  }, 500);
+});
+
+ctaEl.addEventListener('mouseleave', () =>{
+  bSmoothieEl.classList.remove("bounce-blue");
+  oSmoothieEl.classList.remove("bounce-orange");
+});
